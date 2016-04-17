bd798fbb0f927d50ae6c40b678a62a441dd9ed1d
srikargr <sgonuguntlareddy@gmail.com>
2016-04-16 22:09:32 -0500

    backend for default

--- FMSC_Project2016/FMSC_Project2016/Default.aspx.cs
+++ FMSC_Project2016/FMSC_Project2016/Default.aspx.cs
@@ -15,7 +15,39 @@ namespace FMSC_Project2016
         
         protected void Page_Load(object sender, EventArgs e)
         {
+            int start_pixel;
+            int end_pixel;
+            string registered_name;
+            int visible_pixel;
 
+            string conStr = ConfigurationManager.ConnectionStrings["projectConnectionString"].ConnectionString;
+
+            SqlConnection dbConnection = new SqlConnection(conStr);
+            SqlCommand sqlcommand;
+            SqlDataReader reader;
+
+            try
+            {
+                dbConnection.Open();
+                string query = "Select top 1 * from purchase_user order by end_pixel desc;";
+                sqlcommand = new SqlCommand(query, dbConnection);
+                reader = sqlcommand.ExecuteReader();
+                if (reader.Read())
+                {
+                    start_pixel = (int)reader["START_PIXEL"];
+                    end_pixel = (int)reader["END_PIXEL"];
+                    registered_name = reader["REGISTERED_NAME"].ToString();
+                    visible_pixel = (int)reader["END_PIXEL"];
+                }
+            }
+            catch
+            {
+                Response.Write("Some error");
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
         }
 
 
