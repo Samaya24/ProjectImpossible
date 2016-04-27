1223ce2d98732a6c75fa97a439dbe4147c6c688f
srikargr <sgonuguntlareddy@gmail.com>
2016-04-26 20:59:38 -0500

    trial and error

---
+++ FMSC_Project2016/FMSC_Project2016/SMSService.cs
@@ -0,0 +1,28 @@
+﻿using FMSC_Project2016.ISUSMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FMSC_Project2016
+{
+    public class SMSService
+    {
+        public string Provider { get; set; }
+        public string Number { get; set; }
+        public string Message { get; set; }
+
+        public SMSService(string provider, string number, string message)
+        {
+            Provider = provider;
+            Number = number;
+            Message = message;
+        }
+
+        public void SendTextMessage(SMSService textMessage)
+        {
+            var sms = new SUSMSClient();
+            sms.sendSMS(textMessage.Provider, textMessage.Number, textMessage.Message);
+        }
+    }
+}
--- FMSC_Project2016/FMSC_Project2016/WebForm1.aspx
+++
@@ -1,9 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FMSC_Project2016.WebForm1" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="TitleContentPlaceHolder" runat="server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="HeadContentPlaceHolder" runat="server">
-</asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="ScriptContentPlaceHolder" runat="server">
-</asp:Content>
-<asp:Content ID="Content4" ContentPlaceHolderID="MainContentPlaceHolder" runat="server">
-</asp:Content>
